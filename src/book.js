45c4d63377049790c01d0d6f1969c8cb92a2a252
Nick Orlov <ialwaysreply@gmail.com>
2022-02-04 16:04:19 -0700

    Finish book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,63 @@
-function createTitle() {
+function createTitle(book) {
+  return 'The ' + book
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return character
+}
+
+function saveReview(message, messageList) {
+  if (messageList.includes(message)) {
+    return
+  } else {
+
+    messageList.push(message)
+  }
   
+  // console.log('ssssssssssssss ', messageList)
+}
+  
+  // return reviews
+
+   
+    // take in the title which is a string and count how many letters it contains and multiply by 20.
+    // We need to count the number of letters in a title?
+  
+
+  function calculatePageCount(title) {
+    return title.length * 20
+  
+}
+
+
+
+function writeBook(bookName, characterName, bookGenre) {
+  var book = {
+    title: bookName,
+    mainCharacter: characterName,
+    pageCount: calculatePageCount(bookName),
+    genre: bookGenre,
+
+  }
+  return book
+}
+
+
+function editBook(bookName) {
+ bookName.pageCount = bookName.pageCount * .75
+
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,22 @@
+function shelfBook(title, shelf) {
+if (shelf.length <= 2) {
 
+  shelf.unshift(title)
+}
+
+}
+
+function unshelfBook(title, shelf) {
+shelf.pop(title)
+}
+
+function listTitles(shelf) {
+var shelf2 = shelfBook(title, shelf)
+return shelf2
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };

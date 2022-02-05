b95ac3729781f188e2dcebfa655e5eb9cd12b0a0
Nick Orlov <ialwaysreply@gmail.com>
2022-02-04 23:17:02 -0700

    Finish the shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -7,16 +7,36 @@ if (shelf.length <= 2) {
 }
 
 function unshelfBook(title, shelf) {
-shelf.pop(title)
+for (var i = 0; i < shelf.length; i++) {
+  if (shelf[i].title.includes(title)) {
+    shelf.splice(i, 1)
+  }
+}
 }
 
 function listTitles(shelf) {
-var shelf2 = shelfBook(title, shelf)
-return shelf2
+  // var titles = 
+
+for (var i = 0; 0 < shelf.length; i++) {
+  return shelf[i].title + ',' + ' ' + shelf[1].title + ',' + ' ' + shelf[2].title
+  
 }
+}
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true
+  
+     }
+    }
+    return false
+  }
+
+
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf
 };
